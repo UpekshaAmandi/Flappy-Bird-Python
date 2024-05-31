9f7378eaf0acb74ca4554e0b970473ec4ab78a94
Upeksha Amandi <upekshaamandi20@gmail.com>
2024-05-31 11:03:04 +0530

    initial commit

--- text.py
+++ text.py
@@ -6,7 +6,7 @@ import random
 import pygame
 
 WIN_WIDTH = 600
-WID_HEIGHT = 800
+WIN_HEIGHT = 800
 
 BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("images","bird1.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("images","bird2.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("images","bird3.png")))]
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("images","pipe.png")))
